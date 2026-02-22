9054093576131b333c3c427b6697a3b1bc3cde15
Ronan Pelot <ronanpelot@gmail.com>
2026-02-22 16:48:47 -0500

    Basic arithmetic added, proper buttons for numbers, addition, subtraction, multiplication, division, and equals.

--- lib/main.dart
+++ lib/main.dart
@@ -1,121 +1,237 @@
+/*
+  Assignment: Calculator App
+  Author: Ronan Pelot
+  Description: 
+*/
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const CalcuatorApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class CalcuatorApp extends StatelessWidget {
+  const CalcuatorApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return MaterialApp(home: HomePage(),);
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomePageState extends State<HomePage> {
+  String _operandOne = '';
+  String _operandTwo = '';
+  String _input = '';
+  String _display = '';
+  String _operator = '';
+  bool _result = false;
 
-  void _incrementCounter() {
+  bool _operatorSelected = false;
+
+  void _calculate() { 
+      if (!_operatorSelected || _operandOne.isEmpty || _operandTwo.isEmpty) return;
+
+      double num1 = double.parse(_operandOne);
+      double num2 = double.parse(_operandTwo);
+
+      double temp = 0.0;
+
+      if (_operator == '+') {
+        temp = num1 + num2;
+      } else if (_operator == '-') {
+        temp = num1 - num2;
+      } else if (_operator == '*') {
+        temp = num1 * num2;
+      } else if (_operator == '/') {
+        temp = num1 / num2;
+      }
+      setState(() {
+        _result = true;
+        _input = temp.toString();
+        _operandOne = '';
+        _operandTwo = '';
+        _operator = '';
+        _operatorSelected = false;
+      });
+  }
+
+  void selectOperator(String operator) {
+    if (_operatorSelected || _input.isEmpty) return;
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _operandOne = _input;
+      _operator = operator;
+      _input = '';
+      _operatorSelected = true;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
           children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Container(
+              width: 300, 
+              height: 100, 
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 33, 37, 63),
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    _input,
+                    style: const TextStyle(
+                      fontSize: 24,
+                      color: Color.fromARGB(255, 24, 129, 112),
+                    ),
+                    textAlign: TextAlign.right,
+                  ),
+
+                  if (_result) 
+                    Text(
+                      '= ${_input.toString()}',
+                      style: const TextStyle(
+                        fontSize: 20,
+                        color: Color.fromARGB(255, 24, 129, 112),
+                      ),
+                      textAlign: TextAlign.right,
+                    ),
+                ],
+              ),
             ),
-          ],
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 8),
+                ElevatedButton(
+                  onPressed: () => setState(() => _input = '${_input}7'),
+                  child: const Text('7'),
+                ),
+                const SizedBox(height: 8),
+                ElevatedButton(
+                  onPressed: () => setState(() => _input = '${_input}4'),
+                  child: const Text('4'),
+                ),
+                const SizedBox(height: 8),
+                ElevatedButton(
+                  onPressed: () => setState(() => _input = '${_input}1'),
+                  child: const Text('1'),
+                ),
+                const SizedBox(height: 8),
+                ElevatedButton(
+                  onPressed: () => setState(() => _input = '${_input}0'),
+                  child: const Text('0'),
+                ),
+              ],
+            ),
+      
+            const SizedBox(width: 12),
+            Column (
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                /*ElevatedButton(
+                  onPressed: () => setState(() => _counter = 5),
+                  child: const Text('+', style: TextStyle(decoration: TextDecoration.underline)),
+                ),
+                const SizedBox(height: 8),*/
+                ElevatedButton(
+                  onPressed: () => setState(() => _input = '${_input}8'),
+                  child: const Text('8'),
+                ),
+                const SizedBox(height: 8),
+                ElevatedButton(
+                  onPressed: () => setState(() => _input = '${_input}5'),
+                  child: const Text('5'),
+                ),
+                const SizedBox(height: 8),
+                ElevatedButton(
+                  onPressed: () => setState(() => _input = '${_input}2'),
+                  child: const Text('2'),
+                ),
+              ],
+            ),
+
+            const SizedBox(width: 12),
+            Column (
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                /*ElevatedButton(
+                  onPressed: () => setState(() => _counter = 1),
+                  child: const Text('%'),
+                ),
+                const SizedBox(height: 8),*/
+                ElevatedButton(
+                  onPressed: () => setState(() => _input = '${_input}9'),
+                  child: const Text('9'),
+                ),
+                const SizedBox(height: 8),
+                ElevatedButton(
+                  onPressed: () => setState(() => _input = '${_input}6'),
+                  child: const Text('6'),
+                ),
+                const SizedBox(height: 8),
+                ElevatedButton(
+                  onPressed: () => setState(() => _input = '${_input}3'),
+                  child: const Text('3'),
+                ),
+                const SizedBox(height: 8),
+                ElevatedButton(
+                  onPressed: () => setState(() => _input = '${_input}.'),
+                  child: const Text('.'),
+                ),
+              ],
+            ),
+
+            const SizedBox(width: 12),
+            Column (
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () => selectOperator('/'),
+                  child: const Text('÷'),
+                ),
+                const SizedBox(height: 8),
+                ElevatedButton(
+                  onPressed: () => selectOperator('*'),
+                  child: const Text('x'),
+                ),
+                const SizedBox(height: 8),
+                ElevatedButton(
+                  onPressed: () => selectOperator('-'),
+                  child: const Text('-'),
+                ),
+                const SizedBox(height: 8),
+                ElevatedButton(
+                  onPressed: () => selectOperator('+'),
+                  child: const Text('+'),
+                ),
+                const SizedBox(height: 8),
+                ElevatedButton(
+                  onPressed: () {
+                      if (_input.isEmpty) return;
+
+                      _operandTwo = _input;
+                      _calculate();
+                  },
+                  child: const Text('='),
+                ),
+              ],
+            ),
+          ]
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
